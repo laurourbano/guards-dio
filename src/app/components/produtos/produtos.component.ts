0b0f2a7d7e5542afd7b6ca532b23300655cc8cc4
Lauro Otavio Urbano <lauro@outlook.com.br>
2024-01-19 21:05:01 -0300

    update

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -2,25 +2,19 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule],
   templateUrl: './produtos.component.html',
-  styleUrl: './produtos.component.scss'
+  styleUrl: './produtos.component.scss',
 })
 export class ProdutosComponent {
-
-  constructor(
-    private service: LoginService,
-    private router: Router
-  ) {
-    
-  }
+  constructor(private service: LoginService, private router: Router) {}
   deslogar() {
     this.service.doLogout();
     this.router.navigate(['/login']);
   }
-
 }
